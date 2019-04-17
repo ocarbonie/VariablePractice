84a98a730ed1c7a44fc9c6c4345fda27892e76ce
Oona Carbonie <carbonie.oona@gmail.com>
2019-04-17 14:20:43 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        //declare and initialize all variables here before we use them
+        String firstName = "Oona";
+        String lastName = "Carbonie";
+        double gpa = 3.85;
+        String movie = "Captain Marvel";
+
+
+        //Use the variables here
+        System.out.println("My name is " + firstName + " " + lastName);
+        System.out.println("My GPA is " + gpa);
+        System.out.println("The movie I saw last is " + movie);
+    }
+
+}
